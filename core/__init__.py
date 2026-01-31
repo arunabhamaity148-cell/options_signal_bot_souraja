420f77c1ad6c78c1a3e5692cee129dfeab233699
arunabhamaity148-cell <arunabhamaity148@gmail.com>
2026-01-31 21:20:59 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,12 +1,11 @@
 """Core module initialization"""
+# Don't import signal_engine here to avoid circular dependency
 from .binance_client import binance_client
 from .websocket_handler import ws_handler
-from .signal_engine import signal_engine
 from .indicators import indicators
 
 __all__ = [
     "binance_client",
     "ws_handler", 
-    "signal_engine",
     "indicators"
 ]
